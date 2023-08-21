81b195ea2adbe369fe4212db27fc5f0c5a8ac5e2
Taecpy <tae08713@gmail.com>
2023-08-21 10:37:52 +0700

    order

--- code/prepare_data.py
+++ code/prepare_data.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import glob
-
+#%%
 input = 'Input//'
 output = 'Output//'
 rawdata = 'Input//Rawdata//'
@@ -11,7 +11,7 @@ adjust_rawdata = 'Input//Adjust_rawdata//'
 # os.mkdir(output)
 # os.mkdir(rawdata)
 # os.mkdir(adjust_rawdata)
-
+#%%
 def r_data():
         rdata = glob.glob(rawdata+"*.xlsx")
         df_list = []
@@ -24,6 +24,13 @@ def r_data():
         A_data.drop(columns=['Row No'],inplace=True)
         return A_data
 
+def data():
+    data = glob.glob(adjust_rawdata+"*.csv")[-1]
+    df = pd.read_csv(data,encoding = 'TIS-620')
+    df = df.iloc[:,[0,3,5,6,8,9,10,11,21,22,23,24,26,27,37,52,54,132,143]]
+    return df
+#%%
 os.chdir("D:/630039/test_Dev_A-score")
 df = r_data()
 # df.to_csv(adjust_rawdata+'rawdata.csv',encoding='TIS-620')
+df = data()
